13786a705171634b58b7941ac9994faaa7f5bbba
Max Beier <mbeier@gwdg.de>
2020-11-03 12:24:40 +0100

    Block 3 - REST-Service-Beispiel hinzugefügt

---
+++ Block 2 - Dynamische Seiten - Backend/todo-express/index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+/* TODOs verwalten
+- C: POST
+- R: GET
+- U: PATCH
+- D: DELETE
+ */
+
+let todos = [
+  {id: 'a', title: 'eins' , isDone: true },
+  {id: 'b', title: 'zwei' , isDone: false },
+  {id: 'c', title: 'drei' , isDone: false },
+];
+
+app.get('/todos', (req, res) => {
+  res.send({ sucess: true, data: todos });
+});
+
+app.post('/todos', (req, res) => {
+  const todo = req.body;
+  todos.push(todo);
+  res.send({ sucess: true, data: todo });
+});
+
+app.patch('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  const index = todos.findIndex(todo => todo.id === id);
+  todos[index] = { ...todos[index], ...req.body };
+  res.send({ sucess: true, data: todos[index] });
+});
+
+app.delete('/todos/:id', (req, res) => {
+  const id = req.params.id;
+  todos = todos.filter(todo => todo.id !== id);
+  res.send({ sucess: true });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000.');
+});
